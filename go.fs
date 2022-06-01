db0630f874d088743f3bd6d9552a7e677158a6c9
Jakob <jkblrnz@gmail.com>
2022-05-31 19:41:09 -0500

    emacs daemon mode

--- go.fs
+++ go.fs
@@ -1,5 +1,5 @@
 : empty		s" ---marker--- marker ---marker---" evaluate ;
-: edit		s" emacs test.fs" system ;
+: edit		s" emacsclient -t test.fs" system ;
 : run		s" test.fs" included ;
 : ecr		edit run ;
 
